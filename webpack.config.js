a85e04b31a0f53abc8e733b7a8ae6c63a7195952
Alexander <strannik11.92@gmail.com>
2018-07-03 15:59:55 +0300

    Initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: './src/index.js',
-    print: './src/print.js'
+    app: './src/index.js'
   },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  mode: "production",
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
@@ -29,7 +41,7 @@ module.exports = {
         use: [
           'file-loader'
         ]
-      } 
+      }
     ]
   }
 };
