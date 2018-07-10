e457c9aa7a557d046c22b4f0039249e82181e1c8
Alexander <strannik11.92@gmail.com>
2018-07-10 15:13:54 +0300

    Add linters

--- src/js/components/math.js
+++
@@ -1,9 +0,0 @@
-export function square(x) {
-	console.log('I get called from math.js!');
-	return x * x;
-}
-
-export function cube(x) {
-	console.log('I get called from math.js!');
-	return x * x * x;
-}
