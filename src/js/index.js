4d23d2f98aaa4455bb58af42f1494d0368625af3
Alexander <strannik11.92@gmail.com>
2018-07-10 15:09:15 +0300

    Cleaning

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,2 @@
-// import $ from 'jquery';
-import {square} from './components/math.js';
-import printMe from './components/print.js';
-
 import 'normalize.css';
 import '../style/main.scss';
-import '../style/style.css';
-
-function component() {
-	var element = document.createElement('pre');
-	element.innerHTML = [
-		'Hello webpack!',
-		'10 cubed is equal to ' + square(10)
-	].join('\n\n');
-	return element;
-}
-
-function sum(...nums) {
-	return nums.reduce((acc, val) => acc + val);
-}
-
-printMe();
-
-console.log($);
-console.log(jQuery);
-
-console.log(`I get called from index.js! ${sum(1, 4, 5, -20, -1, 0)}`);
-
-document.body.appendChild(component());
